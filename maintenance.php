b71576bf254fdfd24a5588925a5d70fc4fc7afc0
semangka.media <semangkamedia@hotmail.com>
2023-10-11 17:09:27 +0700

    new commit

---
+++ maintenance.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
+    <meta name="author" content="AdminKit">
+    <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+
+    <link rel="canonical" href="https://demo-basic.adminkit.io/pages-sign-in.html" />
+
+    <title>Semangkamedia</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="public/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+    <main class="d-flex w-100">
+        <div class="container d-flex flex-column">
+            <div class="row vh-100">
+                <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
+                    <div class="d-table-cell align-middle">
+                        <div class="text-center mt-4">
+                            <img src="assets/img/sm.png" width="275" height="75">
+                            <p class="lead mt-4">This Site is Currently</p>
+                            <h1 class="h1">Under Maintenance!</h1>
+                            <p class="lead">
+                                We Should be Back Shortly.
+                                Thank you for your patience.
+                            </p>
+                        </div>
+                        <div class="text-center mb-3">
+                            <a href="https://wa.me/6287733807998">Call us</a> for detail.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="assets/css/js/app.js"></script>
+
+</body>
+
+</html>
